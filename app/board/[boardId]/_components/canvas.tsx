a8f8615e6b2534ad4a292d845fb10d34af9b8efa
Atul Patidar <48468580+AtulPatidar1709@users.noreply.github.com>
2024-06-12 23:28:01 +0530

    Selection Box

--- app/board/[boardId]/_components/canvas.tsx
+++ app/board/[boardId]/_components/canvas.tsx
@@ -11,6 +11,7 @@ import { useCanRedo, useCanUndo, useHistory, useMutation, useOthersMapped, useSt
 import { CursorsPresence } from "./cursors-presence";
 import { connectionIdToColor, pointerEvnetToCanvasPoint } from "@/lib/utils";
 import { LayerPreview } from "./layerPreview";
+import { SelectionBox } from "./selectionBox";
 
 const MAX_LAYERS = 100;
 
@@ -193,6 +194,9 @@ export const Canvas = ({ boardId }: CanvasProps) => {
                             selectionColor={layerIdsToColorSelection[layerId]}
                         />
                     ))}
+                    <SelectionBox
+                        onResizeHandlePointerDown={() => { }}
+                    />
                     <CursorsPresence />
                 </g>
             </svg>
